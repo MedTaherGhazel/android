673e1a1a70a684a2323ca435977ecad204423d68
Med Taher Ghazel <119009624+MedTaherGhazel@users.noreply.github.com>
2023-10-16 23:24:25 +0100

    Tp 2 popup

--- app/src/main/java/com/example/tp2/MainActivity.kt
+++ app/src/main/java/com/example/tp2/MainActivity.kt
@@ -10,17 +10,25 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Button
+import android.widget.PopupMenu
 import android.widget.TextView
 import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btn :Button
+    lateinit var btn: Button
+    lateinit var btnpop: Button
+    lateinit var bg: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn= findViewById(R.id.btn)
+        btn = findViewById(R.id.btn)
+        btnpop = findViewById(R.id.pop)
+        bg = findViewById(R.id.layout)
         registerForContextMenu(btn);
+        btnpop.setOnClickListener() {
+            showPopupMenu(btnpop)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -29,23 +37,28 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onCreateContextMenu(menu: ContextMenu?, v: View?,
-                                     menuInfo: ContextMenu.ContextMenuInfo?) {
+    override fun onCreateContextMenu(
+        menu: ContextMenu?, v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
         super.onCreateContextMenu(menu, v, menuInfo)
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.color, menu)
 // menu.setHeaderTitle("ContextMenu");
     }
+
     override fun onContextItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-                R.id.pressme1 ->{
-                    btn.setBackgroundColor(Color.RED)
-                    return true
-                }
-                R.id.pressme2-> {
-                    btn.setBackgroundColor(Color.BLUE)
-                    return true
-                }
+            R.id.pressme1 -> {
+                btn.setBackgroundColor(Color.RED)
+                return true
+            }
+
+            R.id.pressme2 -> {
+                btn.setBackgroundColor(Color.BLUE)
+                return true
+            }
+
             else -> super.onContextItemSelected(item)
         }
         return false
@@ -53,21 +66,61 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id. menu_search -> {
-                Toast.makeText(getApplicationContext(),"menu search selectionné" , Toast.LENGTH_LONG).show()
+            R.id.menu_search -> {
+                Toast.makeText(
+                    getApplicationContext(),
+                    "menu search selectionné",
+                    Toast.LENGTH_LONG
+                ).show()
                 true
             }
-            R.id.menu_exit-> {
-                Toast.makeText(getApplicationContext(),"menu exit selectionné" , Toast.LENGTH_LONG).show()
+
+            R.id.menu_exit -> {
+                Toast.makeText(getApplicationContext(), "menu exit selectionné", Toast.LENGTH_LONG)
+                    .show()
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
 
 
+    private fun showPopupMenu(btn: Button) {
+        val popupMenu = PopupMenu(this, btn)
+        val inflater = popupMenu.menuInflater
+        inflater.inflate(R.menu.popmenu, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.menu_rouge -> {
+                    bg.setBackgroundColor(Color.parseColor("#FFA2A2"))
+                    btn.setBackgroundColor(Color.RED)
+                    true
+                }
 
+                R.id.menu_bleu -> {
+                    bg.setBackgroundColor(Color.parseColor("#A2D2FF"))
+                    btn.setBackgroundColor(Color.BLUE)
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        popupMenu.show()
     }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
